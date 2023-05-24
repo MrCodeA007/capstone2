5f1c081206e99210ede16bd9ec9f101a071ec229
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-24 08:49:21 -0400

    table displaying

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,57 @@
-`use strict`
+`use strict`;
 
-const  searchSelectorEl = document.getElementById(`searchSelector`);
+const searchSelectorEl = document.getElementById(`searchSelector`);
 function dropdownSelector() {
-    for (i = 0; i <locationsArray.length; i++) {
-    let optionEl =document.createElement("option");
+  for (i = 0; i < locationsArray.length; i++) {
+    let optionEl = document.createElement("option");
     const locationAreas = locationsArray[i];
     optionEl.textContent = locationAreas;
     searchSelectorEl.appendChild(optionEl);
+  }
 }
-}
-dropdownSelector();
-//---------------------------------------------------------
+//--------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------
 function onsearchSelectorClickedChanged() {
-    let selectedValue =searchSelectorEl.value;
-    const selectedstate = nationalParksArray.filter(
-    (statelocale) => statelocale.State === selectedValue
-);
-console.log(selectedstate); worked
-seletectedstate.forEach((park) => {
-    const row = tbody.insertRow();
-    
-    const cellLocale = row.insertCell();
-    cellLocale.innerHTML = park.LocationName;
+    //one selected state or value
+  let selectedValue = searchSelectorEl.value;
+  
+  //an array of parks
+  const selectedparks = nationalParksArray.filter(
+    // (park) => park.State === selectedValue (arrow function method)
+    function(park) {
+        return park.State === selectedValue;
+    }
+  );
 
-}
-) 
-}
+  //console.log(selectedparks) worked;
+selectedparks.forEach((park) => {
+    const row = tableEl.insertRow();
 
-searchSelectorEl.onchange = onsearchSelectorClickedChanged;
+    const cellLocationName = row.insertCell();
+    cellLocationName.innerHTML = park.LocationName;
+
+    const cellAddress = row.insertCell();
+    cellAddress.innerHTML = park.Address;
+
+    const cellCity = row.insertCell();
+    cellCity.innerHTML = park.City;
+
+    const cellState = row.insertCell();
+    cellState.innerHTML = park.State;
+
+    const cellZipCode = row.insertCell();
+    cellZipCode.innerHTML = park.ZipCode;
+
+    const cellPhone = row.insertCell();
+    cellPhone.innerHTML = park.Phone;
+});
+  } 
+
+//-----------------------------------------------------------------------------
+
+const tableEl = document.getElementById(`nationalParkTable`)
+searchSelectorEl.onchange = onsearchSelectorClickedChanged; 
+dropdownSelector();
 
 
