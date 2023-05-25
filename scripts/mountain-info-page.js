987a18160f2e77dd9a73b184c7939f03a3c12620
Alejandro Regus <Aregus@appdev.yearup.org>
2023-05-25 10:29:39 -0400

    mountain table displaying selected mountains-needs img and table bug fixes

--- scripts/mountain-info-page.js
+++ scripts/mountain-info-page.js
@@ -9,5 +9,63 @@ function dropdownmountainSelector() {
     mountainSelectorEl.appendChild(optionEl);
   }
 }
+//-------------------------------------------------------------------------
+function onmountainSelectorClickedChanged() {
+  //one selected state or value
+mountainSelectorEl.addEventListener('change', () => {
+  const tbodyEl = document.querySelector("#mountainDataTable");
+  tbodyEl.innerHTML = "";
+
+
+let selectedValue = mountainSelectorEl.value;
+
+//an array of parks
+const selectedMountains = mountainsArray.filter(
+  // (park) => park.State === selectedValue (arrow function method)
+  function(mountains) {
+      return mountains.name === selectedValue;
+  }
+);
+
+//console.log(selectedparks) worked;
+selectedMountains.forEach((mountains) => {
+
+  const row = tbodyEl.insertRow();
+
+  const cellLocationName = row.insertCell();
+  cellLocationName.innerHTML = mountains.name;
+
+  const cellDescription = row.insertCell();
+  cellDescription.innerHTML = mountains.desc;
+
+  const cellElevation = row.insertCell();
+  cellElevation.innerHTML = mountains.elevation; 
+
+  // const cellState = row.insertCell();
+  // cellState.innerHTML = park.State;
+
+  // const cellZipCode = row.insertCell();
+  // cellZipCode.innerHTML = park.ZipCode;
+
+  // const cellPhone = row.insertCell();
+  // cellPhone.innerHTML = park.Phone;
+
+  // if (park.Address === 0) {
+  //   cellAddress.innerHTML = "Not available";
+  // }
+  // if (park.Phone === 0 ) {
+  //   cellPhone.innerHTML = "Not available"; 
+  // }
+  // if (park.ZipCode === 0) {
+  //   cellZipCode.innerHTML = "Not available";
+  // }
+});
+});
+} 
+
+//-----------------------------------------------------------------------------
+
+const tbodyEl = document.getElementById(`mountainDataTable`)
+mountainSelectorEl.onchange = onmountainSelectorClickedChanged; 
 
 dropdownmountainSelector();
